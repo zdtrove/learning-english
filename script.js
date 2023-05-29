2182b6b8c5666d0e9f8b490aca61a9ee13aefeb6
Tôn Nguyễn Hoàng Ân <41717215+zdtrove@users.noreply.github.com>
2023-05-29 16:52:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,15 +6,32 @@ let i;
 
 data.forEach((item, index) => {
     const h2 = document.createElement("h2");
-    h2.innerHTML = `${index + 1}.${item.title}`;
+    h2.innerHTML = `${index + 1}. ${item.title}`;
     listWord.appendChild(h2);
     const div = document.createElement("div");
     div.classList.add("item");
     item.list.forEach((item1, index1) => {
         const word = document.createElement("p");
         const example = document.createElement("p");
+        const button = document.createElement("button");
+        const meaning = document.createElement("b");
+        meaning.classList.add("meaning");
+        meaning.style.display = "none";
+        meaning.innerHTML = `<i>${item1.meaning}</i>`;
+        button.addEventListener('click', function handleClick(event) {
+            if (meaning.style.display === "none") {
+                meaning.style.display = "inline-block";
+                button.innerHTML = '<span>-</span>';
+            } else {
+                meaning.style.display = "none";
+                button.innerHTML = '<span>+</span>';
+            }
+        });
+        button.innerHTML = '<span>+</span>';
         example.classList.add("example");
-        word.innerHTML = `${index1 + 1}.${item1.word}`;
+        word.innerHTML = `${index1 + 1}. ${item1.word}`;
+        word.appendChild(button);
+        word.appendChild(meaning);
         example.innerHTML = item1.example;
         div.appendChild(word);
         div.appendChild(example);
