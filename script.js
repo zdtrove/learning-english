9365afc9710c28df9eb6abdbcbdddf184504ff36
Tôn Nguyễn Hoàng Ân <41717215+zdtrove@users.noreply.github.com>
2023-05-29 14:21:34 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 import data from './data.json' assert { type: 'json' };
 
 const listWord = document.getElementsByClassName("list-word")[0];
-data.forEach(item => {
+const h2 = document.getElementsByTagName("h2");
+let i;
+
+data.forEach((item, index) => {
     const h2 = document.createElement("h2");
-    h2.innerHTML = item.title;
+    h2.innerHTML = `${index + 1}.${item.title}`;
     listWord.appendChild(h2);
     const div = document.createElement("div");
     div.classList.add("item");
-    item.list.forEach((item1) => {
+    item.list.forEach((item1, index1) => {
         const word = document.createElement("p");
         const example = document.createElement("p");
         example.classList.add("example");
-        word.innerHTML = `<p>${item1.word}</p>`;
-        example.innerHTML = `<p class="example">- ${item1.example}</p>`;
+        word.innerHTML = `${index1 + 1}.${item1.word}`;
+        example.innerHTML = item1.example;
         div.appendChild(word);
         div.appendChild(example);
         listWord.appendChild(div);
     });
 });
 
-const h2 = document.getElementsByTagName("h2");
-let i;
-
 for (i = 0; i < h2.length; i++) {
     h2[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
+        if (panel.style.opacity == 1) {
+            panel.style.opacity = 0;
+            panel.style.height = 0;
         } else {
-            panel.style.display = "block";
+            panel.style.opacity = 1;
+            panel.style.height = "auto";
         }
     });
 }
